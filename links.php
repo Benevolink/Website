97469dd7cfc96bbf390709bdc5b9b09835d213c2
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-25 14:31:50 +0200

    correction prb js

--- links.php
+++ links.php
@@ -11,7 +11,6 @@ $path = __DIR__.'/';
 require_once __DIR__."/db.php";
 //Importation de la bibliothèque BF
 require_once __DIR__.'/functions/basic_functions.php';
-require_once BF::abs_path("/JS/abs_path.php",true);
 BF::sess_start();
 if(!isset($a_jour)){
   $a_jour = false;
