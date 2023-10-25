5021ee0433ab03cac3c7ddfb03602815ac6b33bd
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-25 14:21:42 +0200

    erreur

--- controller/static/homepage.php
+++ controller/static/homepage.php
@@ -6,5 +6,6 @@ require_once __DIR__.'/../../links.php';
 require_once BF::abs_path("libs/User.php",true);
 require_once BF::abs_path("model/".$file_name,true);
 require_once BF::abs_path("view/".$file_name,true);
+
 BF::afficher_template(ob_get_clean())
 ?>
