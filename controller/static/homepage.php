5941c026d21f7b3bff3db70b3e8f04c5c3896101
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-25 14:10:29 +0200

    correction erreur

--- controller/static/homepage.php
+++ controller/static/homepage.php
@@ -4,6 +4,7 @@ ob_start();
 $file_name = "static/".basename(__FILE__);
 require_once __DIR__.'/../../links.php';
 require_once BF::abs_path("libs/User.php",true);
+require_once BF::abs_path("model/".$file_name,true);
 require_once BF::abs_path("view/".$file_name,true);
 BF::afficher_template(ob_get_clean())
 ?>
