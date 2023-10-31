841f7566f99ed0c18cb62c3d331b28dd85ec3967
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-31 16:56:40 +0100

    Correction bug interfaces PHP inférieur à 7.1

--- libs/noms_tables.php
+++ libs/noms_tables.php
@@ -1,10 +1,14 @@
 <?php
-
-
-/**
- * Table evenements
+ /**
+ * Définit le nom de toutes les tables
+ * permet de rapidemment pouvoir changer le nom des tables sans afftecter le code
  */
- interface AttributsEvents{
+
+ class AttributsTables
+ {
+    /**
+     * Table evenements
+     */
     public const EVENT              =   "evenements";
     public const EVENT_ID           =   "id_event";
     public const EVENT_ID_ASSO      =   "id_asso";
@@ -15,12 +19,12 @@
     public const EVENT_NB_MAX_PERSONNES
                                     =   "nb_personnes";
     public const EVENT_ID_HORAIRE   =   "id_horaire";
- }
 
-/**
- * Table associations
- */
- interface AttributsAssos{
+
+    /**
+     * Table associations
+     */
+
     public const ASSO = "assos";
     public const ASSO_ID = "id";
     public const ASSO_NOM = "nom";
@@ -30,31 +34,26 @@
     public const ASSO_EMAIL = "email";
     public const ASSO_TELEPHONE = "tel";
     public const ASSO_LOGO = "logo";
- }
-
-/**
- * Table domaines
- */
-interface AttributsDomaines{
+ 
+    /**
+     * Table domaines
+     */
     public const DOMAINE = "domaine";
     public const DOMAINE_ID = "id_domaine";
     public const DOMAINE_NOM = "nom_domaine";
-}
 
-/**
- * Table de jonction domaines
- */
-interface AttributsDomainesJonction{
+
+    /**
+     * Table de jonction domaines
+     */
     public const DOMAINEJONCTION = "domaine_jonction";
     public const DOMAINEJONCTION_ID_DOMAINE = "id_domaine";
     public const DOMAINEJONCTION_ID_JONCTION = "id_jonction";
     public const DOMAINEJONCTION_TYPE = "type";
-}
 
-/**
- * Table des horaires
- */
-interface AttributsHoraires{
+    /**
+     * Table des horaires
+     */
     public const HORAIRE = "horaire";
 
     public const HORAIRE_ID = "id_horaire";
@@ -64,74 +63,55 @@ interface AttributsHoraires{
     public const HORAIRE_DATE_FIN = "date_fin";
     public const HORAIRE_HEURE_DEBUT = "heure_debut";
     public const HORAIRE_HEURE_FIN = "heure_fin";
-}
-
-/**
- * Table des lieux
- */
-interface AttributsLieux{
+    /**
+     * Table des lieux
+     */
     public const LIEU = "lieu";
     public const LIEU_ID = "id_lieu";
     public const LIEU_DEPARTEMENT = "departement";
     public const LIEU_ADRESSE = "adresse";
-}
-
-/**
- * Table des membres des associations
- */
-interface AttributsMembresAssos{
+    /**
+     * Table des membres des associations
+     */
     public const MEMBRESASSOS = "membres_assos";
     public const MEMBRESASSOS_ID_USER = "id_user";
     public const MEMBRESASSOS_ID_ASSO = "id_asso";
     public const MEMBRESASSOS_STATUT = "statut";
-}
-
-/**
- * Table des membres des évènements
- */
-interface AttributsMembresEvents{
+    /**
+     * Table des membres des évènements
+     */
     public const MEMBRESEVENTS = "membres_evenements";
     public const MEMBRESEVENTS_ID_EVENT = "id_event";
     public const MEMBRESEVENTS_ID_USER  = "id_user";
     public const MEMBRESEVENTS_STATUT = "statut";
-}
-
-/**
- * Table de propriétés des associations
- */
-interface AttributsPropAsso{
+    
+    /**
+     * Table de propriétés des associations
+     */
     public const PROPASSO = "prop_assos";
     public const PROPASSO_ID_ASSO = "id_asso";
     public const PROPASSO_NOM = "prop_nom";
-    public const PROPASSO_VALEUR = "valeur";
-}
-
-/**
- * Table de propriétés des associations
- */
-interface AttributsPropEvent{
+    
+   public const PROPASSO_VALEUR = "valeur";
+   /**
+     * Table de propriétés des associations
+     */
     public const PROPEVENT = "prop_evenements";
     public const PROPEVENT_ID_EVENT = "id_event";
     public const PROPEVENT_NOM = "prop_nom";
     public const PROPEVENT_VALEUR = "valeur";
-}
-
-/**
- * Table des signalements
- */
-interface AttributsSignalement{
+    /**
+     * Table des signalements
+     */
     public const SIGNALEMENT = "signalements";
     public const SIGNALEMENT_ID_SOURCE = "id_source";
     public const SIGNALEMENT_ID_CIBLE = "id_cible";
     public const SIGNALEMENT_MESSAGE = "message";
     public const SIGNALEMENT_DATE = "date_signal";
     public const SIGNALEMENT_EST_ASSO  = "est_une_asso";
-}
-
-/**
- * Table des utilisateurs
- */
-interface AttributsUsers{
+    /**
+    * Table des utilisateurs
+    */ 
     public const USER = "users";
     public const USER_ID = "id";
     public const USER_NOM = "nom";
@@ -144,37 +124,6 @@ interface AttributsUsers{
     public const USER_PRENOM = "prenom";
     public const USER_ETAT_COMPTE = "account_status";
     public const USER_LOGO = "logo";
-}
-
-
-/**
- * Table horaires
- */
-
-
- /**
- * Définit le nom de toutes les tables
- * permet de rapidemment pouvoir changer le nom des tables sans afftecter le code
- */
-
- class AttributsTables
- implements 
- AttributsAssos,
- AttributsDomaines,
- AttributsDomainesJonction,
- AttributsDomainesJonction,
- AttributsEvents,
- AttributsHoraires,
- AttributsLieux,
- AttributsMembresAssos,
- AttributsMembresEvents,
- AttributsPropAsso,
- AttributsPropEvent,
- AttributsSignalement,
- AttributsUsers
- {
-    
-
  }
 
 ?>
