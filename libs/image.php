919a9c8612017be6ab972aaa5f35f01ccb6284bf
PhelixPheneon <148976639+PhelixPheneon@users.noreply.github.com>
2023-11-07 15:05:46 +0100

    oui

--- libs/image.php
+++ libs/image.php
@@ -20,65 +20,70 @@ class Image {
      * Permet de rentrer de manière sécurisée un image dans la base de donnée 
      * @return void
      */
+
+    
+    
     public static function placer_image($image,$table,$chemin){
         $unique = 0;
         $ext = pathinfo($image, PATHINFO_EXTENSION);
-        
-        //Début vérification qu'il s'agit d'un bon format
-       
-        //Vérifier l'extension
-        $allowed_extensions = array('jpg', 'jpeg', 'png', 'gif');
-        $nom_image=$_FILES['uploadedfile']['name'];
-        $extension = pathinfo($nom_image, PATHINFO_EXTENSION);
-        
-        if (!in_array($extension, $allowed_extensions)) {
-            
-            return 1; // changer en echo ?
-            }
-            //Vérifier qu'il n'y a pas de points dans le nom de l'image
-            
-        for ($i=0; $i<strlen($nom_image)-5; $i++) {
-              
-            if ($nom_image[$i]==".") {
-            return 2; //changer en echo ?
-            }
-        }
-      //Si pas de return le format est le bon        
-
-        //Fin vérification qu'il s'agit d'un bon format
 
 
         while($unique!=1){
             $image_name = uniqid();
 
-            $req_id = "SELECT logo FROM".$table."WHERE basename(logo)=? ";//on vérifie que le nom n'est pas déjà pris
+            $req_id = "SELECT logo FROM".$table." WHERE basename(logo)=? ";//on vérifie que le nom n'est pas déjà pris
             $req_id_2 = $db->prepare($req_id);
             $req_id_2->execute([$image_name]);
             $id = $req_id_2->fetch(PDO::FETCH_ASSOC);
+
             if (count($id) == 0) {
       //donc si le nom est bien unique on peut juste sortir de la boucle
                 $unique = 1;
             }
         }
+
   //Mettre l'image dans le fichier logo/user/
-        $destinationPath = $path.$chemin.basename($_FILES['uploadedfile']['name']); 
-        array_map('unlink', glob($path.$chemin.$image_name.".*")); //On supprime les fichiers résiduels
+        $destinationPath = $chemin.basename($_FILES['uploadedfile']['name']); 
+        array_map('unlink', glob($chemin.$image_name.".*")); //On supprime les fichiers résiduels
         if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $destinationPath)) {
             echo "Le fichier ".  basename( $_FILES['uploadedfile']['name'])." a bien été téléversé";
-        } 
+        } //la fonction move_uploaded_file déplace le fichier dans destination et renvoie un si l'opération est un succés 0 sinon
         else{
             echo "Il y a eu une erreur pour poster le fichier, réessayez.";
         }
         $newDestinationPath = $chemin.$image_name.".".$ext;
-        rename($destinationPath, $path.$newDestinationPath);
+        //rename($destinationPath, $newDestinationPath);
         //UPDATE le chemin vers l'image dans la BDD
-        BF::request("UPDATE assos SET logo = ? WHERE id = ?",[$newDestinationPath,$id]);
+        BF::request("UPDATE" .$table. " SET logo = ? WHERE id = ?",[$newDestinationPath,$id]);
         
  
   
   
 }
-
+    public static function verifier_format($image){
+        $allowed_extensions = array('jpg', 'jpeg', 'png', 'gif');
+        $nom_image=$_FILES['uploadedfile']['name'];
+        $extension = pathinfo($nom_image, PATHINFO_EXTENSION);
+    
+    //on vérifie le format de l'image
+        if (!in_array($extension, $allowed_extensions)) {
+        
+        echo "Cette extension n'est pas authorisée";
+        }
+        //Vérifier qu'il n'y a pas de points dans le nom de l'image
+        
+        for ($i=0; $i<strlen($nom_image)-5; $i++) {
+          
+            if ($nom_image[$i]==".") {
+            echo "Il ne peut y avoir d'autres points que celui de l'extension"; 
+             }
     }
+}
+    /*public static function corriger_extension($image){
+        $ext = pathinfo($image, PATHINFO_EXTENSION);
+
+    }*/
+
+}
 
 ?>
