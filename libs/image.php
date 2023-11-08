2f4edea98b6c0574f21a650b06d304a24f0f9f67
PhelixPheneon <148976639+PhelixPheneon@users.noreply.github.com>
2023-11-08 15:37:36 +0100

    Modifications des fonctions pour fonction avec un objet de classe image au lieu d'utiliser la variable globale "$FILE"

--- libs/image.php
+++ libs/image.php
@@ -5,9 +5,15 @@ require_once BF::abs_path("db.php",true);
 
 class image {
     
-    
+    private $name;
+    private $type;
+    private $size;
+    private $tmp_name;
+    private $error;
+    private $fullpath;
 
     public static function getImage(){}
+    public static function setImage(){}
     
 
   
@@ -16,7 +22,7 @@ class image {
     /**
      * Method placer_image
      *
-     * @param $image $image [est le nom d'origine de l'image. Les vraies manipulations de l'image se font via "$_FILEs"]
+     * @param $image $image [est "l'image" donc un objet de la classe image]
      * @param $table $table [la table où le chemin de l'asso sera stockée]
      * @param $chemin $chemin [chemin depuis la racine où l'on souhaite que l'image soit stockée]
      *
@@ -27,33 +33,42 @@ class image {
     /*
     *A été modifié : à retester
     */
+    /*Deuxième modif : la fonction prends un argument $image qui est un objet de la classe image qui a été créé à partir des données 
+    récupérées dans FILE. De ce que j'ai compris après le chargement de l'image pas la méthode POST l'image est stockée quelque part
+    et toutes les fonctions permettant de déplacer une image et tout prennent en argument le nom de l'imgage puis vont la chercher. 
+    Le champs tmp_nom est le champs contenant le nom de l'image sur le serveur (celui qui nous intéresse) et le champs name contient 
+    le nom d'origine de l'image (il ne nous intéresse pas)*/ 
 
     public function placer_image($image,$table,$chemin){
         
 
         $unique = 0;
-        $ext = pathinfo($image, PATHINFO_EXTENSION);
+        $ext = pathinfo($image->tmp_name, PATHINFO_EXTENSION);
 
 
         while($unique!=1){
-            $image_name = uniqid();
+            $image_name_num = uniqid();
 
-            $req_id = "SELECT logo FROM".$table." WHERE basename(logo)=? ";//on vérifie que le nom n'est pas déjà pris
-            $req_id_2 = $db->prepare($req_id);
-            $req_id_2->execute([$image_name]);
-            $id = $req_id_2->fetch(PDO::FETCH_ASSOC);
+            $req_noms = "SELECT logo FROM".$table." WHERE basename(logo)=? ";//on vérifie que le nom n'est pas déjà pris
+            $req_noms_2 = $db->prepare($req_id);
+            $req_noms_2->execute([$image_name_num]);
+            $id = $req_noms_2->fetch(PDO::FETCH_ASSOC);
 
             if (count($id) == 0) {
       //donc si le nom est bien unique on peut juste sortir de la boucle
                 $unique = 1;
             }
         }
+        //changer le nom
+        $image_name=strval($image_name_num);
+        rename($image->tmp_name,$image_name);
+        $image->tmp_name=$image_name;
 
-  //Mettre l'image dans le fichier logo/user/
+        //Mettre l'image dans le fichier logo/user/
         $destinationPath = $chemin.$image_name.".".$ext;
         array_map('unlink', glob($chemin.$image_name.".*")); //On supprime les fichiers résiduels
-        if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $destinationPath)) { 
-            echo "Le fichier ".  basename( $_FILES['uploadedfile']['name'])." a bien été téléversé";
+        if(move_uploaded_file($image->tmp_name, $destinationPath)) { 
+            echo "Le fichier ".  basename( $image->name)." a bien été téléversé";
         } //la fonction move_uploaded_file déplace le fichier dans destination et renvoie un si l'opération est un succés 0 sinon
         else{
             echo "Il y a eu une erreur pour poster le fichier, réessayez.";
@@ -61,7 +76,7 @@ class image {
         
         //rename($destinationPath, $newDestinationPath);
         //UPDATE le chemin vers l'image dans la BDD
-        BF::request("UPDATE" .$table. " SET logo = ? WHERE id = ?",[$newDestinationPath,$id]);
+        BF::request("UPDATE" .$table. " SET logo = ? WHERE id = ?",[$destinationPath,$id]);
         
  
   
@@ -69,30 +84,23 @@ class image {
 }
     public static function verifier_format($image){
         $allowed_extensions = array('jpg', 'jpeg', 'png');
-        $nom_image=$_FILES['uploadedfile']['name'];
-        $extension = pathinfo($nom_image, PATHINFO_EXTENSION);
+       
+        $extension = pathinfo($image->tmp_name, PATHINFO_EXTENSION);
     
     //on vérifie le format de l'image
         if (!in_array($extension, $allowed_extensions)) {
         
         echo "Cette extension n'est pas authorisée";
         }
-        //Vérifier qu'il n'y a pas de points dans le nom de l'image
+        //Vérifier qu'il n'y a pas de points autres que celui de l'extension dans le nom de l'image pour éviter une double extension
         
-        for ($i=0; $i<strlen($nom_image)-5; $i++) {
+        for ($i=0; $i<strlen($image->tmp_name)-5; $i++) {
           
             if ($nom_image[$i]==".") {
             echo "Il ne peut y avoir d'autres points que celui de l'extension"; 
              }
     }
 }
-    public static function corriger_extension($image){
-        $ext = pathinfo($image, PATHINFO_EXTENSION);
-        if(strcmp($ext,"jpg")!=0){
-
-        }
-
-    }
 
 }
 
