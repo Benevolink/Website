ef9a792534388b3d510750f9d57e1a3df205e9c5
riken95 <ebraud@gmail.com>
2023-11-08 13:30:38 +0000

    commit de paulo

--- view/search.php
+++ view/search.php
@@ -1,3 +1,8 @@
+ <!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
 <?php
 if(BF::is_posted("Recherche")){
   $r = $_GET["Recherche"];
@@ -9,7 +14,8 @@ $r = $r."%";
 $table = BF::request("SELECT * FROM assos WHERE nom LIKE ? ORDER BY nom ASC",[$r],true);
 ?>
 <link rel="stylesheet" href="<?= BF::abs_path("CSS/search.css") ?>">
-<table id="list_assos">
+<table class="table table-striped table-bordered" id="list_assos">
+
   
 <?php
 if($table == NULL){
