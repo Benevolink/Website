edf102b6721b76a58def0be06bfab6951af2a763
Anne-So59 <148352404+Anne-So59@users.noreply.github.com>
2023-11-10 14:40:25 +0100

    fonction display_asso : affiche nom, logo et desc

--- JS/classes/User.js
+++ JS/classes/User.js
@@ -4,69 +4,52 @@ export class User{
         this.id_user = id_user;
     }
 
-    afficher_profil_court(){
-        //ajax pour récupérer le user à partir de l'id puis affichage
-        $.ajax({
+    getInfo(info_name){
+        return $.ajax({
             url: abs_path("NOMFICHIER.php"),
             method: "POST",
             dataType: "json",
             data: {
-                id_user: this.id_user
+                id_asso: this.id_asso
             }
-        }).done(function(rep){
+        }).then(function(rep) {
             console.log(rep);
-            //détailler l'affichage paramètre par paramètre :
-            //nom, prenom, logo
-            var nom_user = rep.nom;
-            var prenom_user = rep.prenom;
-            var logo_user = rep.logo;
-
-            //afficher bien HTML
-            main.append(
-                $('<div>').css({
-                    cursor: "pointer",
-                    margin: "10px",
-                    backgroundColor: "rgb(245,245,245)",
-                    width: "600px",
-                    borderRadius: "3px",
-                    padding: "10px"
-                })
-                //ajouter un on click : afficher en grand
-            );
-
-        })
-        
+            return rep[info_name];
+        });
     }
+        
+    getNom(){
+        this.getInfo("nom");
+    }    
 
-    afficher_profil_long(reponse){
-        //affichage dans une page à part on click
-        //on part de la rep et on affiche un peu comme details courts avec des trucs en plus/+grand
+    getPrenom(){
+        this.getInfo("prenom");
+    }    
 
-        //détailler l'affichage paramètre par paramètre : nom, prenom, date_de_naissance, email, tel, logo, id_lieu, account_status
-        var nom_user = reponse.nom;
-        var prenom_user = reponse.prenom;
-        var logo_user = reponse.logo;
-        var date_de_naissance_user = reponse.date_de_naissance;
-        var email_user = reponse.email;
-        var tel_user = reponse.tel;
-        var id_lieu_user = reponse.id_lieu;
-        var account_status = reponse.account_status;
+    getLogo(){
+        this.getInfo("logo");
+    }    
+
+    getDateNaissance(){
+        this.getInfo("date_de_naissance");
+    }    
+
+    getEmail(){
+        this.getInfo("email");
+    }    
+
+    getTel(){
+        this.getInfo("tel");
+    }    
+
+    getIdLieu(){
+        this.getInfo("id_lieu");
+    }    
+
+    getAccountStatus(){
+        this.getInfo("account_status");
+    }       
         
         //a partir de id_lieu : déterminer département
-              
-
-        //afficher bien HTML
-        main.append(
-            $('<div>').css({
-                cursor: "pointer",
-                margin: "10px",
-                backgroundColor: "rgb(245,245,245)",
-                width: "600px",
-                borderRadius: "3px",
-                padding: "10px"
-            })
-            //ajouter un on click : afficher en grand
-        );
-    }
-
+             
 }
