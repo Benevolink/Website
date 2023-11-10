aee1f53e639161f43800652092053203cc3d33cc
Anne-So59 <148352404+Anne-So59@users.noreply.github.com>
2023-11-10 14:10:19 +0100

    méthodes getInfo classes Event et User

--- JS/classes/Event.js
+++ JS/classes/Event.js
@@ -4,63 +4,49 @@ export class Event{
         this.id_event = id_event;
     }
 
-    afficher_event_court(){
-        //ajax pour récupérer infos event puis affichage
-        $.ajax({
+    getInfo(info_name){
+        return $.ajax({
             url: abs_path("NOMFICHIER.php"),
             method: "POST",
             dataType: "json",
             data: {
-                id_event: this.id_event
+                id_asso: this.id_asso
             }
-        }).done(function(rep){
+        }).then(function(rep) {
             console.log(rep);
-            //détailler l'affichage paramètre par paramètre : nom_event
-            var nom_event = rep.nom_event;
-
-            //afficher bien HTML
-            main.append(
-                $('<div>').css({
-                    cursor: "pointer",
-                    margin: "10px",
-                    backgroundColor: "rgb(245,245,245)",
-                    width: "600px",
-                    borderRadius: "3px",
-                    padding: "10px"
-                })
-                //ajouter un on click : afficher en grand
-            );
-        })
-        
+            return rep[info_name];
+        });
     }
 
-    afficher_event_long(reponse){
-        //affichage dans une page à part on click
-        //on part de la rep et on affiche un peu comme details courts avec des trucs en plus/+grand
-
-        //détailler l'affichage paramètre par paramètre : nom_event, desc, visu, id_lieu, nb_personnes, id_horaire, id_domaine
-        var nom_event_event = reponse.nom_event;
-        var desc_event = reponse.desc_event;
-        var visu_event = reponse.visu;
-        var id_lieu_event = reponse.lieu_event;
-        var nb_personnes_event = reponse.nb_personnes;
-        var id_horaire_event = reponse.id_horaire;
-        var id_domaine_event = reponse.id_domaine;
-
-        //a partir de id_lieu, id_horaire, id_domaine : déterminer département, date_debut/heure_debut, nom_domaine
-
-        //afficher bien HTML
-        main.append(
-            $('<div>').css({
-                cursor: "pointer",
-                margin: "10px",
-                backgroundColor: "rgb(245,245,245)",
-                width: "600px",
-                borderRadius: "3px",
-                padding: "10px"
-            })
-            //ajouter un on click : afficher en grand
-        );
+    getName(){
+        return this.getInfo("nom_event");
     }
+
+    getDesc(){
+        return this.getInfo("desc_event");
+    }
+
+    getVisu(){
+        return this.getInfo("visu");
+    }
+
+    getIdlieu(){
+        return this.getInfo("id_lieu");
+    }
+
+    getNbPersonnes(){
+        return this.getInfo("nb_personnes");
+    }
+
+    getIdHoraire(){
+        return this.getInfo("id_horaire");
+    }
+
+    getIdDomaine(){
+        return this.getInfo("id_domaine");
+    }
+
+    //a partir de id_lieu, id_horaire, id_domaine : déterminer département, date_debut/heure_debut, nom_domaine
+
 
 }
