387d3966ca68fe90523e5cb9bff46e08e04a796f
riken95 <128236100+riken95@users.noreply.github.com>
2023-11-13 09:14:34 +0100

    controller evenement.php prise en compte du cas id_event non isset

--- controller/evenement.php
+++ controller/evenement.php
@@ -4,6 +4,9 @@ if(isset($_GET["iframe"]) ? 1 : 0){
     $iframe = true;
 }
 require_once __DIR__.'/../links.php';
+if(!isset($_GET["id_event"])){
+    BF::go_home();
+}
 
 require_once BF::abs_path("libs/Event.php",true);
 $event = new Event($_GET["id_event"]);
