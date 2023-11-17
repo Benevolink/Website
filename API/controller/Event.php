bd984c0d6678ad7b56281d61109f8e6c28a6e4df
riken95 <128236100+riken95@users.noreply.github.com>
2023-11-17 23:27:53 +0100

    Début changement liste missions

--- API/controller/Event.php
+++ API/controller/Event.php
@@ -1,9 +1,16 @@
 <?php
-require_once BF::abs_path("libs/Asso.php",true);
-require_once __DIR__."/../model/Asso.php";
+require_once BF::abs_path("libs/Event.php",true);
+require_once __DIR__."/../model/Event.php";
 
 switch($fonction){
     case "":
         break;
+    case "user_get_all":
+        require_once BF::abs_path("libs/User.php",true);
+        $user = new User();
+        return_json($user->infos_events());
+        exit();
+
+
 
 }
