32e47cdc4d2d07a55bade8a038c3fced0baa14a4
riken95 <128236100+riken95@users.noreply.github.com>
2023-11-17 23:03:45 +0100

    fix table vide search

--- controller/search.php
+++ controller/search.php
@@ -12,7 +12,6 @@ else
 require_once BF::abs_path("libs/Asso.php",true);
 $table = Asso::recherche_asso($r);
 ob_start();
-require_once BF::abs_path("functions/categories.php",true);
 require_once BF::abs_path("view/".$file_name,true);
 BF::afficher_template();
 ?>
