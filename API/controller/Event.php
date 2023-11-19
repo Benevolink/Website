e953bf341f3f1799f7be6802a379dfedfce632b7
riken95 <128236100+riken95@users.noreply.github.com>
2023-11-19 12:53:20 +0100

    API PHP creation event

--- API/controller/Event.php
+++ API/controller/Event.php
@@ -10,7 +10,18 @@ switch($fonction){
         $user = new User();
         return_json($user->infos_events());
         exit();
-
+    case "insert":
+        try{
+            $array = json_decode($_POST["array"]); 
+            APIEvent::insert($array["date_debut"],$array["date_fin"],$array["heure_debut"],$array["heure_fin"],$array["id_asso"],$array["nom_event"],$array["nb_personnes"],$array["visu"],$array["desc"],$array["departement"],$array["adresse"]);
+            return_statut(true);
+        }
+        catch(Exception $e){
+            return_statut(false,$e->getMessage());  
+        }
+        exit();
+        
+        
 
 
 }
