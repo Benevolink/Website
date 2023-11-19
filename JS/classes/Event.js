97a8234c8f4d0528aa605e3d179679cec479885b
riken95 <128236100+riken95@users.noreply.github.com>
2023-11-19 12:58:43 +0100

    API JS insert event

--- JS/classes/Event.js
+++ JS/classes/Event.js
@@ -12,6 +12,23 @@ export class Event extends APIObjet{
         .APICall("event","get_all",{id_event : this.id});
     }
 
+    static insert(date_debut,date_fin,heure_debut,heure_fin,id_asso,nom_event,nb_personnes,visu,desc,departement,adresse){
+        array = {
+            date_debut : date_debut,
+            date_fin : date_fin,
+            heure_debut : heure_debut,
+            heure_fin : heure_fin,
+            id_asso : id_asso,
+            nom_event : nom_event,
+            nb_personnes : nb_personnes,
+            visu : visu,
+            desc : desc,
+            departement : departement,
+            adresse : adresse
+        };
+        return this
+        .APICall("event","insert",{array : array});
+    }
 
     //a partir de id_lieu, id_horaire, id_domaine : déterminer département, date_debut/heure_debut, nom_domaine
 
