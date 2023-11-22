32172ee54b8cf81099dd6a002ba7532b2e7a5394
PhelixPheneon <148976639+PhelixPheneon@users.noreply.github.com>
2023-11-22 16:59:31 +0100

    creation des cas suppr_compte et deconnexion

--- API/controller/User.php
+++ API/controller/User.php
@@ -25,25 +25,25 @@ switch($fonction){
         exit();
     case "set_pp":
         $user = new APIUser();
-        $user->id=$_POST["id_user"];
+        $user->id=$_SESSION["id_user"];
         $user->set_user_image($_POST["photo_profil"]);
         return_statut(true);
         exit();
     case "change_pp":
         $user = new APIUser();
-        $user->id=$_POST["id_user"];
+        $user->id=$_SESSION["id_user"];
         $user->change_user_image($_POST["photo_profil"]);
         return_statut(true);
         exit();
     case "suppr_pp":
         $user = new APIUser();
-        $user->id=$_POST["id_user"];
+        $user->id=$_SESSION["id_user"];
         $user->suppr_image_user();
         return_statut(true);
         exit();
     case "get_pp":
         $user = new APIUser();
-        $user->id=$_POST["id_user"];
+        $user->id=$_SESSION["id_user"];
         $array = array("statut" => 1,"lien_image" => $user->get_image_user());
         return_json($array);
         exit();
