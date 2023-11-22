5417ef4f361a9bd7cee7cfcc3c16018060a4f9ad
Anne-So59 <148352404+Anne-So59@users.noreply.github.com>
2023-11-22 15:43:26 +0100

    modifs createAsso

--- JS/after/asso/admin/creation_asso.js
+++ JS/after/asso/admin/creation_asso.js
@@ -1,7 +1,8 @@
 function createAsso(nom, logo, desc, desc_missions, adresse, tel, email){
-    rep = this.APICall("asso","insert",
+    asso = new Asso();
+    rep = asso.APICall("asso","insert",
     {nom : nom, logo : logo, desc : desc, desc_missions : desc_missions, adresse : adresse, tel : tel, email : email});
-    if(rep["statut"]==0){
+    if(rep["status"]==0){
         
     }
     else{
