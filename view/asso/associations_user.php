e7674007973e4f7b9404c7158480a4fa27100621
encorpaulo <paulo_cayanan@hotmail.fr>
2023-11-29 16:24:24 +0100

    changement disposition mon profil + mes associations
    quelques petites retouches pour harmoniser la page
    
    retouche de mes asociations

--- view/asso/associations_user.php
+++ view/asso/associations_user.php
@@ -30,6 +30,10 @@
     #sous_titre{
       text-align: center;
     }
+
+    #bouton_creation_asso{
+      text-align: center;
+    }
   </style>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 </head>
@@ -41,10 +45,18 @@
 
     <div class="row">
       <h2 id="titre_asso">Mes associations</h3>
-
-      <button onclick="location.href = 'admin/creation_asso.php';" type="button" class="btn btn-default btn-lg">
+<br>
+      <div class="row">
+  <div class="col-md-6 col-md-offset-1">
+    <button onclick="location.href = 'admin/creation_asso.php';" type="button" class="btn btn-default btn-lg" id="bouton_creation_asso">
       <span class="glyphicon glyphicon-star" aria-hidden="true"></span> Je veux créer mon association
     </button>
+  </div>
+</div>
+
+<br>
+<br>
+
 
       <div class="row search-bar">
       <div class="col-md-6 col-md-offset-3">
