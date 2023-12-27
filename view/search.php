6b030be1f1dcc3141916c584efb19e409423059a
riken95 <128236100+riken95@users.noreply.github.com>
2023-12-27 22:16:13 +0100

    On enlèvre boostrap importé en double

--- view/search.php
+++ view/search.php
@@ -1,9 +1,3 @@
- <!-- Latest compiled and minified JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
-
 <link rel="stylesheet" href="<?= BF::abs_path("CSS/search.css") ?>">
 <table class="table table-striped table-bordered" id="list_assos">
 
