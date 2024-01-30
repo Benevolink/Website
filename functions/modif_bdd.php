fddfbe6ba8de81655f9df899d51300760bd61239
PhelixPheneon <148976639+PhelixPheneon@users.noreply.github.com>
2024-01-30 16:27:45 +0100

    Correction base de donnee

--- functions/modif_bdd.php
+++ functions/modif_bdd.php
@@ -3,12 +3,14 @@
 //on va commencer par altérer les tables déjà existantes (User et Event) puis on créera JoinHoraire
 
 //Modification User
+global $db;
 
 $sql_user = "ALTER TABLE users 
              ADD dist_max_accepte INT
              ADD duree_max_accepte INT";
 
-$exec_sql_user->exec($sql_user);
+$sql_user_prep = $db->prepare($sql_user);
+$sql_user_prep->execute();
 
 //Modification table lieu
 
@@ -16,7 +18,8 @@ $sql_lieu = "ALTER TABLE lieu
             ADD coordonee_x INT
             ADD coordonee_y INT";
 
-$exec_sql_lieu->exec($sql_lieu);
+$sql_lieu_prep = $db->prepare($sql_lieu);
+$sql_lieu_prep->execute();
 
 //Modification evenement
 
@@ -24,7 +27,8 @@ $sql_event = "ALTER TABLE evenements
               ADD duree_mission INT
               ADD indice_prio_mission INT";
 
-$exec_sql_event->exec($sql_event);
+$sql_event_prep = $db->prepare($sql_event);
+$sql_event_prep->execute();
 //Création join_horaire
 
 $sql_joinhoraire = "CREATE TABLE join_horaire (
@@ -33,7 +37,8 @@ $sql_joinhoraire = "CREATE TABLE join_horaire (
     id_join INT
 )";
 
-$exec_sql_joinhoraire->exec($sql_joinhoraire);
+$sql_joinhoraire_prep = $db->prepare($sql_joinhoraire);
+$sql_joinhoraire_prep->execute();
 
 
 ?>
